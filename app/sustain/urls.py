92bcec721ff9670b2baa45a72233a2969066abaf
Ronan Murphy <ronan.murphy@originmarkets.com>
2018-09-15 23:42:06 +0100

    Fixing buffer size, log location and module for uwsgi. Added localhost as allowed host in django and added some dummy endpoints.

--- app/sustain/urls.py
+++ app/sustain/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from glue import views as glue_views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", glue_views.hello_world),
+    path("moon/", glue_views.hello_moon),
 ]
